7c9dbe850bb704b33bf1834468b40d0c207d239f
Isaac Enmanuel Perez <20130824@ce.pucmm.edu.do>
2016-10-29 20:59:23 -0400

    Initial commit

---
+++ src/main/java/Clases/grupo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author isaac
+ */
+public class grupo {
+    private String Codigo;
+    private String Nombre;
+    private Date FechaInicio;
+    private Date FechaFin;
+    private taller Tipo;
+    private String CodigoEntrenador;
+    private Integer Cupo;
+    private Float Precio;
+
+
+
+
+
+
+
+}
